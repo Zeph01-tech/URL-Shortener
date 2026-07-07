622516828f2bd58fe1c038121cf278459b17551b
Yash Raj Dubey <yashrajdubey32@gmail.com>
2026-07-07 21:37:02 +0530

    added frontend folder with files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return <>
+  <div className='flex lol justify-center items-center'>
+    <div className=''>
+      Hi
+    </div>
+  </div>
+  </>
+}
+
+export default App
